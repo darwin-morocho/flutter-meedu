4cb55f40abd33873e53c2e32a506f075932fa126
Darwin Morocho <www.dina.ec@gmail.com>
2021-05-26 12:50:50 -0500

    added SimpleTagProvider and StateTagProvider

---
+++ packages/meedu/test/providers/state_tag_provider_test.dart
@@ -0,0 +1,29 @@
+import 'package:meedu/provider.dart';
+import 'package:test/test.dart';
+
+import '../state/state_controller_test.dart';
+
+void main() {
+  setUp(() {
+    _stateTagProvider.clear();
+  });
+  test('StateTagProvider', () {
+    expect(_stateTagProvider.count, 0);
+    final provider1 = _stateTagProvider.find('1');
+    final provider2 = _stateTagProvider.find('2');
+    expect(_stateTagProvider.count, 2);
+    provider1.read.onEmailChanged("test@test.com");
+
+    expect(provider1.read.state.email, "test@test.com");
+    expect(provider2.read.state.email, "");
+
+    provider1.dispose();
+    expect(_stateTagProvider.count, 1);
+    provider2.dispose();
+    expect(_stateTagProvider.count, 0);
+  });
+}
+
+final _stateTagProvider = StateProvider.withTag(
+  (ref) => LoginController(),
+);
